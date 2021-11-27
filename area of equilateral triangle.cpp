fa746ea598542be2fcfc56b28548873a268291e1
OlisettyVivek <93900880+OlisettyVivek@users.noreply.github.com>
2021-11-27 21:33:32 +0530

    Add files via upload

---
+++ area of equilateral triangle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int b; float a;
+	printf("enter the value of base");
+	scanf("%d",&b);
+	a=(b*b*1.732)/4;
+	printf("the area of triangle is %f",a);
+	return 0;
+}
