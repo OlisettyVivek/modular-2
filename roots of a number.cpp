3b9f2aaae26520881ea72f787184b579c3a8e685
OlisettyVivek <93900880+OlisettyVivek@users.noreply.github.com>
2021-11-27 22:35:30 +0530

    Add files via upload

---
+++ roots of a number.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	double num,root;
+	printf("enter any number to find square root");
+	scanf("%lf",&num);
+	root= sqrt(num);
+	printf("square root of %.2lf=%.2lf",num,root);
+	return 0;
+}
