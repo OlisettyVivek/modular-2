54fa3bd361b68281cb5ca4934e41b9a0bc19032c
OlisettyVivek <93900880+OlisettyVivek@users.noreply.github.com>
2021-12-18 13:54:35 +0530

    Add files via upload

---
+++ perfect number.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,rem,sum=0;
+	printf("enter a number");
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{ rem=n%i;
+	if(rem==0)
+	sum+=i;
+	}
+if(sum==n)
+printf("%d is a perfect number",n);
+elsee  
+printf("%d is not a perfect number",n);
+}
